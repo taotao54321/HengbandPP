1ee1ecaa4cff79b8ca9431a84b9fd9c76805cae8
taotao54321 <taotao54321@gmail.com>
2021-01-17 05:54:03 +0900

    upd

--- src/term/z-term.h
+++ src/term/z-term.h
@@ -8,16 +8,14 @@
  * are included in all such copies.
  */
 
-#ifndef INCLUDED_Z_TERM_H
-#define INCLUDED_Z_TERM_H
+#pragma once
 
-#include "system/angband.h"
 #include "system/h-basic.h"
 
 /*!
  * @brief A term_win is a "window" for a Term
  */
-typedef struct term_win {
+struct term_win {
     bool cu, cv; //!< Cursor Useless / Visible codes
     TERM_LEN cx, cy; //!< Cursor Location (see "Useless")
 
@@ -32,13 +30,12 @@ typedef struct term_win {
 
     TERM_COLOR* vta; //!< Note that the row of attr at(0, y) is a[y]
     char* vtc; //!< Note that the row of chars at(0, y) is c[y]
-} term_win;
+};
 
 /*!
  * @brief term実装構造体 / An actual "term" structure
  */
-typedef struct term_type term_type;
-typedef struct term_type {
+struct term_type {
     vptr user; //!< Extra "user" info (used by application)
     vptr data; //!< Extra "data" info (used by implementation)
 
@@ -91,9 +88,9 @@ typedef struct term_type {
     errr (*bigcurs_hook)(TERM_LEN x, TERM_LEN y); //!< 大型タイル時カーソル描画実装部 / Hook for placing the cursor on bigtile mode
     errr (*wipe_hook)(TERM_LEN x, TERM_LEN y, int n); //!< 指定座標テキスト消去実装部 / Hook for drawing some blank spaces
     errr (*text_hook)(TERM_LEN x, TERM_LEN y, int n, TERM_COLOR a, concptr s); //!< テキスト描画実装部 / Hook for drawing a string of chars using an attr
-    void (*resize_hook)(void); //!< 画面リサイズ実装部
+    void (*resize_hook)(); //!< 画面リサイズ実装部
     errr (*pict_hook)(TERM_LEN x, TERM_LEN y, int n, const TERM_COLOR* ap, concptr cp, const TERM_COLOR* tap, concptr tcp); //!< タイル描画実装部 / Hook for drawing a sequence of special attr / char pairs
-} term_type;
+};
 
 /**** Available Constants ****/
 
@@ -145,7 +142,7 @@ void term_queue_bigchar(TERM_LEN x, TERM_LEN y, TERM_COLOR a, char c, TERM_COLOR
 
 void term_queue_line(TERM_LEN x, TERM_LEN y, int n, TERM_COLOR* a, char* c, TERM_COLOR* ta, char* tc);
 
-errr term_fresh(void);
+errr term_fresh();
 errr term_set_cursor(int v);
 errr term_gotoxy(TERM_LEN x, TERM_LEN y);
 errr term_draw(TERM_LEN x, TERM_LEN y, TERM_COLOR a, char c);
@@ -155,8 +152,8 @@ errr term_addstr(int n, TERM_COLOR a, concptr s);
 errr term_putch(TERM_LEN x, TERM_LEN y, TERM_COLOR a, char c);
 errr term_putstr(TERM_LEN x, TERM_LEN y, int n, TERM_COLOR a, concptr s);
 errr term_erase(TERM_LEN x, TERM_LEN y, int n);
-errr term_clear(void);
-errr term_redraw(void);
+errr term_clear();
+errr term_redraw();
 errr term_redraw_section(TERM_LEN x1, TERM_LEN y1, TERM_LEN x2, TERM_LEN y2);
 
 errr term_get_cursor(int* v);
@@ -164,14 +161,14 @@ errr term_get_size(TERM_LEN* w, TERM_LEN* h);
 errr term_locate(TERM_LEN* x, TERM_LEN* y);
 errr term_what(TERM_LEN x, TERM_LEN y, TERM_COLOR* a, char* c);
 
-errr term_flush(void);
+errr term_flush();
 errr term_key_push(int k);
 errr term_inkey(char* ch, bool wait, bool take);
 
-errr term_save(void);
-errr term_load(void);
+errr term_save();
+errr term_load();
 
-errr term_exchange(void);
+errr term_exchange();
 
 errr term_resize(TERM_LEN w, TERM_LEN h);
 
@@ -185,6 +182,4 @@ errr term_putstr_v(TERM_LEN x, TERM_LEN y, int n, byte a, concptr s);
 
 #ifndef WINDOWS
 errr term_nuke(term_type* t);
-#endif
-
 #endif
