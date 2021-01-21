7dbdb5cefea7f6918434df5fd2b640fd478bfc24
taotao54321 <taotao54321@gmail.com>
2021-01-21 15:53:11 +0900

    comment

--- src/prelude.hpp
+++ src/prelude.hpp
@@ -32,20 +32,27 @@ using u64 = std::uint64_t;
 using f32 = float;
 using f64 = double;
 
+// boost::irange() が first > last の場合に壊れる問題を回避するラッパー
 template <class Integer>
 inline auto IRANGE(const Integer first, const Integer last) {
     return boost::integer_range<Integer>(std::min(first, last), last);
 }
 
+// IRANGE(0, last) と等価
 template <class Integer>
 inline auto IRANGE(const Integer last) {
     return IRANGE(static_cast<Integer>(0), last);
 }
 
+// ループカウンタなしで n 回ループ
 #define LOOP(n) for ([[maybe_unused]] std::add_const_t<std::remove_cv_t<std::remove_reference_t<decltype(n)>>> BOOST_PP_CAT(macro_loop_counter, __COUNTER__) : IRANGE(n))
 
+// f(begin(c), end(c), ...) と等価
 #define ALL(f, c, ...) (([&](decltype((c)) cccc) { using std::begin, std::end; return (f)(begin(cccc), end(cccc), ##__VA_ARGS__); })(c))
 
+// choose minimum
+// x < xmin ならば xmin = x と更新する
+// 更新があったかどうかを返す
 template <class T, class Comp = std::less<>>
 inline bool chmin(T& xmin, const T& x, Comp comp = {}) {
     if (comp(x, xmin)) {
@@ -55,6 +62,9 @@ inline bool chmin(T& xmin, const T& x, Comp comp = {}) {
     return false;
 }
 
+// choose maximum
+// x > xmax ならば xmax = x と更新する
+// 更新があったかどうかを返す
 template <class T, class Comp = std::less<>>
 inline bool chmax(T& xmax, const T& x, Comp comp = {}) {
     if (comp(xmax, x)) {
@@ -112,6 +122,8 @@ inline void EPRINTLN_IMPL(const S& format_str, Args&&... args) {
     WRITELN_IMPL(std::cerr, format_str, std::forward<Args>(args)...);
 }
 } // namespace detail
+
+// Rust の同名マクロの模造品
 #define FORMAT(s, ...) detail::FORMAT_IMPL(FMT_STRING(s), ##__VA_ARGS__)
 #define WRITE(out, s, ...) detail::WRITE_IMPL((out), FMT_STRING(s), ##__VA_ARGS__)
 #define WRITELN(out, s, ...) detail::WRITELN_IMPL((out), FMT_STRING(s), ##__VA_ARGS__)
@@ -126,6 +138,9 @@ template <class S, class... Args>
     throw std::logic_error(FORMAT("[{}:{}]: PANIC: {}", file, line, FORMAT_IMPL(format_str, std::forward<Args>(args)...)));
 }
 } // namespace detail
+
+// Rust の panic! 模造品
+// 例外を用いて実装しているので、C と C++ が混在する環境では C 側に例外が漏れないよう注意すること
 #define PANIC(s, ...) detail::PANIC_IMPL(__FILE__, __LINE__, FMT_STRING(s), ##__VA_ARGS__)
 
 namespace detail {
@@ -139,4 +154,6 @@ inline void DBG_IMPL(const std::string_view file, const int line, const std::str
     EPRINTLN("[{}:{}]: ({}) = {}\n", file, line, expr, std::forward_as_tuple(values...));
 }
 } // namespace detail
+
+// Rust の dbg! 模造品
 #define DBG(...) detail::DBG_IMPL(__FILE__, __LINE__, #__VA_ARGS__, __VA_ARGS__)
