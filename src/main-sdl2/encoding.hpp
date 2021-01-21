9fecec356c36eb72186052be27dcbfa2925c67d1
taotao54321 <taotao54321@gmail.com>
2021-01-21 17:52:13 +0900

    upd

--- src/main-sdl2/encoding.hpp
+++ src/main-sdl2/encoding.hpp
@@ -6,19 +6,3 @@
 #include "main-sdl2/prelude.hpp"
 
 std::string euc_to_utf8(const std::string& euc);
-
-#if 0
-// EUC-JP を1文字読み進め、その文字を返す。
-// 2~3 バイト文字についてはリトルエンディアンで返す。
-//
-// 呼び出し後、it は次の文字を指す。
-// it == last で呼び出してはならない。
-template <class InputIt>
-u32 euc_next(InputIt& it, InputIt last) {
-    assert(it != last);
-
-    const u8 b1 = *it++;
-
-    if (b1 <= 0x7F) return b1;
-}
-#endif
